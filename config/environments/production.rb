b5d542dad67d1219aae7ec93ceaffe95458c64d6
Joe-Pumpkin <jonathan.lalanne@outlook.fr>
2020-11-03 20:57:32 +0100

    hehe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.action_mailer.perform_deliveries = false
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'eventbrite-online.herokuapp.com' }
 end
