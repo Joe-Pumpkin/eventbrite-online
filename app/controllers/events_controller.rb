d5f90d05158a7fa4abe64fd45f8e5c2403d1ccd1
Joe-Pumpkin <jonathan.lalanne@outlook.fr>
2020-11-04 12:52:37 +0100

    Test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
   def index
     @event = Event.all
   end
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(params_event)
+    @event.event_admin_id = current_user.id
+
+    if @event.save
+      redirect_to event_path(@event.id)
+      flash[:success] = "Ton évenement a bien été créé !"
+    else render 'new'
+    end
+  end
+
+  private
+
+  def params_event
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :right_user
+
   def show
     @user = User.find(params[:id])
+    @event_admin = Event.all
   end
+
+private
+
+  def right_user
+    @user = User.find(params[:id])
+    if current_user.id == @user.id
+    else
+      redirect_to root_path
+  end
+end
 end
